909220d15229416f6e9a745254e13f3e7d36d80b
Devscreations <rajkhushisingh2@gmail.com>
2024-06-11 05:00:53 +0530

    Update generate.py

--- Gagan/generate.py
+++ Gagan/generate.py
@@ -34,7 +34,7 @@ import config
 
 
 
-ask_ques = "**» ▷ Hi👋, I AM STRING SESSION BOT 👇 : :**"
+ask_ques = "**» Hi👋, I AM STRING SESSION BOT 👇 : :**"
 buttons_ques = [
     [
         InlineKeyboardButton("🔰Generate your string session..", callback_data="pyrogram"),
