c17157d2224a020b8aad4c013b3bb1d959b2f6bc
Devscreations <rajkhushisingh2@gmail.com>
2024-06-14 01:10:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ BOT_TOKEN = "7411202018:AAFuVrnxb0X27OTa-UOJfsMksBacVKoHaZE"
 OWNER_ID = "5128979564"
 
 MONGO_DB_URI = "mongodb+srv://knight_rider:GODGURU12345@knight.jm59gu9.mongodb.net/?retryWrites=true&w=majority"
-MUST_JOIN = "rajcourse"
+MUST_JOIN = "English_Spoken_with_Nitu_mam"
